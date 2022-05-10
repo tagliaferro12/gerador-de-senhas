a4f3c9fb312a9c1f2bc6edef6b9df9636d1da99f
Matheus <matheusjpg12@gmail.com>
2022-05-10 16:39:34 -0400

    oi

---
+++ script.js
@@ -0,0 +1,37 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector('#button');
+
+let sizePassword = document.querySelector('#valor');
+let password = document.querySelector('#password');
+
+let containerPassword = document.querySelector('#container-password');
+
+let charset = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!$*";
+let novaSenha = "";
+
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+
+function generetePassword(){
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+           pass += charset.charAt(Math.floor(Math.random() * n));
+        }
+    containerPassword.classList.remove("hide");   
+    password.innerHTML = pass;
+
+}
+
+function copyPassword(){
+    //alert("Senha copiada com sucesso!")
+    swal("Boa!","Sua senha foi copiada com sucesso!", "success");
+    navigator.clipboard.writeText(novaSenha);
+}
+
+
